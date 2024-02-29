00c5e030fcf4ac1634678b4f6c0fee19b9e93d22
rugile-dd <rugile@dripdrop.io>
2024-02-29 16:02:20 +0100

    Uploading the changes that were made after forking repository
    Modified extension to provide only function lines.
    Modified output file to be in CSV format and contain file name the function is located and should be tested and test id fields.

--- README.md
+++ README.md
@@ -1,35 +1,14 @@
-# List Symbols [![Visual Studio Marketplace Installs - Azure DevOps Extension](https://img.shields.io/visual-studio-marketplace/azure-devops/installs/total/jmbeach.list-symbols)](https://marketplace.visualstudio.com/items?itemName=jmbeach.list-symbols) [![Visual Studio Marketplace Rating (Stars)](https://img.shields.io/visual-studio-marketplace/stars/jmbeach.list-symbols)](https://marketplace.visualstudio.com/items?itemName=jmbeach.list-symbols)
+# listFunctionNamesCSV
 
-![List Symbols Icon](src/assets/icon.png)
+This extension will list all function/method names in a code file in CSV using vscode's built-in symbol processing capabilities.
 
-This extension will list all symbols in a code file as plain text using vscode's built-in symbol processing capabilities.
+### Usage:
+1. Get function/method names of one specific file:
+	- Select a file to retrieve function names for
+	- Run commands: `ctrl + shift + p` -> `List Function Names`
 
-Simply run `ctrl + shift + p` -> `List Symbols`
+2. Get function/method names of a folder
+	Right click a folder and select `List All Function Names in Folder` to get a combined output of all of the files in that folder.
 
-![Example usage](./demo.png)
-
-and you will get output like the following in a new tab (Based on [this example code](https://github.com/WebDevSimplified/Vanilla-JavaScript-Calculator)):
-
-```
-class Calculator
-	constructor constructor
-	method appendNumber
-	method chooseOperation
-	method clear
-	method compute
-		variable computation
-		variable current
-		variable prev
-	method delete
-	method getDisplayNumber
-		variable decimalDigits
-		variable integerDigits
-		variable integerDisplay
-		property maximumFractionDigits
-		variable stringNumber
-	method updateDisplay
-```
-
-## List All Symbols in Folder
-
-Also, you can right click a folder and select `List All Symbols in Folder` to get a combined output of all of the files in that folder.
+### Reference 
+The extension was created by modifying GitHub user jmbeach repository: https://github.com/jmbeach/vscode-list-symbols.git 
--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,40 @@
 import * as vscode from 'vscode';
 import SymbolKinds from './SymbolKinds';
 
+function processNodesMulti(symbols: vscode.DocumentSymbol[], depth: number, lines: string[], filename: string): string {
+  let result = '';
+  for (const symbol of symbols) {
+    const tabs = [...new Array(depth)].reduce((a, b) => a + '\t', '');
+    let line = lines[symbol.selectionRange.start.line];
+
+    if(SymbolKinds[symbol.kind] == 'method' || SymbolKinds[symbol.kind] == 'function'){
+      if(line.includes(',')){
+        line = "\"" + line + "\"";
+      }
+      line = line.replace("\n", "");
+      line = line.trim();
+      result += `${filename},${line},TRUE,""\n`;
+    }
+    if (symbol.children) {
+      result += processNodesMulti(symbol.children, depth + 1, lines, filename);
+    }
+  }
+  return result;
+}
+
 function processNodes(symbols: vscode.DocumentSymbol[], depth: number, lines: string[]): string {
   let result = '';
   for (const symbol of symbols) {
     const tabs = [...new Array(depth)].reduce((a, b) => a + '\t', '');
-    const privateReg = /\bprivate\b/;
-    const publicReg = /\bpublic\b/;
     const line = lines[symbol.selectionRange.start.line];
-    const privacy = privateReg.exec(line) ? 'private ' : publicReg.exec(line) ? 'public ' : '';
-    result += `${tabs}${privacy}${SymbolKinds[symbol.kind]} ${symbol.name}\n`;
+ 
+    if(SymbolKinds[symbol.kind] == 'method' || SymbolKinds[symbol.kind] == 'function'){
+      result += `${line}\n`;
+    }
     if (symbol.children) {
       result += processNodes(symbol.children, depth + 1, lines);
     }
   }
-
   return result;
 }
 
@@ -28,7 +48,7 @@ const getRelativeFilePath = (uri: vscode.Uri) => {
 };
 
 export function activate(context: vscode.ExtensionContext) {
-	context.subscriptions.push(vscode.commands.registerCommand('extension.listSymbols', () => {
+	context.subscriptions.push(vscode.commands.registerCommand('extension.listFunctionNames', () => {
     if (!vscode.window.activeTextEditor) {
       vscode.window.showWarningMessage('There must be an active text editor');
       return;
@@ -45,10 +65,9 @@ export function activate(context: vscode.ExtensionContext) {
       });
 	}));
 
-  context.subscriptions.push(vscode.commands.registerCommand('extension.listAllSymbolsInFolder', (fileMeta) => {
+  context.subscriptions.push(vscode.commands.registerCommand('extension.listAllFunctionNamesInFolder', (fileMeta) => {
     const folderPath = getRelativeFilePath(fileMeta);
 
-    // TODO: Make a config so that users can choose to exclude files with a glob
     vscode.workspace.findFiles(`${folderPath}/**`, undefined, undefined).then(uris => {
       const promises = [];
       for (const uri of uris) {
@@ -72,10 +91,17 @@ export function activate(context: vscode.ExtensionContext) {
       Promise.all(promises).then(allSymbols => {
         let fullText = '';
         const filtered = allSymbols.filter(s => typeof s !== 'undefined') as { symbols: vscode.DocumentSymbol[]; fileUri: vscode.Uri; fileText: string; }[];
+        fullText += `File path,Function name,Should be tested,Test ID\n`
         for (const fileSymbols of filtered) {
-          fullText += `${getRelativeFilePath(fileSymbols.fileUri)}\n---\n`;
-          fullText += processNodes(fileSymbols.symbols, 0, fileSymbols.fileText.split('\n'));
-          fullText += '\n';
+          let filename = getRelativeFilePath(fileSymbols.fileUri);
+
+          let testDirRegex = new RegExp('src\/tests\/');
+          if(!testDirRegex.test(filename)){
+            let functionsFound = processNodesMulti(fileSymbols.symbols, 0, fileSymbols.fileText.split('\n'), filename);
+            if(functionsFound != ''){
+              fullText += processNodesMulti(fileSymbols.symbols, 0, fileSymbols.fileText.split('\n'), filename);
+            }
+          }        
         }
 
         vscode.workspace.openTextDocument({ content: fullText }).then(doc => {
